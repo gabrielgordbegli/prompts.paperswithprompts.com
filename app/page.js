432df5cc21f1519ae8ed811e69d0be49d8a7a029
gabrielgordbegli <ggordbegli@gmail.com>
2023-04-26 17:07:50 -0400

    save

--- app/page.js
+++ app/page.js
@@ -1,9 +1,42 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
+
+
+  function generateThumbnails() {
+    function generateImageFileNames() {
+      const imageFileNames = [];
+      for (let i = 0; i <= 26; i++) {
+        imageFileNames.push(`${i}.png`);
+      }
+      return imageFileNames;
+    }
+    const imageFileNames = generateImageFileNames();
+    return imageFileNames.map((fileName, index) => {
+      return (
+        <>
+          <img key={index} className={styles['thumbnail']} key={fileName} alt='prompt image' src={`/images/${fileName}`} />
+        </>
+      )
+    });
+  }
+
+  const thumbNails = generateThumbnails();
+
   return (
-    <main className={styles['flex']}>
-    </main>
+    <>
+      <div className={styles['flex']}>
+        {
+          thumbNails.map((thumbnail, index) => {
+            return (
+              <>
+                {thumbnail}
+              </>
+            )
+          })
+        }
+      </div>
+    </>
+
   )
 }
